fc29c224c867f7984dd60ad20132681bbcbe6509
keta1 <ketal@qq.com>
2021-10-31 12:35:01 +0800

    update HostInformationProvider

--- app/src/main/java/me/kyuubiran/qqcleaner/data/HostInformationProvider.kt
+++ app/src/main/java/me/kyuubiran/qqcleaner/data/HostInformationProvider.kt
@@ -1,22 +1,13 @@
 package me.kyuubiran.qqcleaner.data
 
 import android.app.Application
-import android.content.Context
-import android.content.pm.PackageInfo
-import android.content.pm.PackageManager
-import android.os.Build
-import com.github.kyuubiran.ezxhelper.utils.Log
 import me.kyuubiran.qqcleaner.utils.HostApp
-import java.util.*
+import java.util.Locale
 
 data class HostInformationProvider(
     val application: Application,
     val packageName: String,
-    val hostName: String,
-    val versionCode: Long,
-    val versionCode32: Int,
-    val versionName: String,
-    val isTim: Boolean
+    val hostName: String
 )
 
 lateinit var hostApp: HostApp
@@ -27,18 +18,13 @@ lateinit var hostInfo: HostInformationProvider
 
 fun init(applicationContext: Application) {
     if (::hostInfo.isInitialized) throw IllegalStateException("Host Information Provider has been already initialized")
-    val packageInfo = getHostInfo(applicationContext)
     val packageName = applicationContext.packageName
     hostInfo = HostInformationProvider(
         applicationContext,
         packageName,
-        applicationContext.applicationInfo.loadLabel(applicationContext.packageManager).toString(),
-        getLongVersionCode(packageInfo),
-        getLongVersionCode(packageInfo).toInt(),
-        packageInfo.versionName,
-        "com.tencent.tim" == packageName
+        applicationContext.applicationInfo.loadLabel(applicationContext.packageManager).toString()
     )
-    when (hostInfo.hostName.toLowerCase(Locale.ROOT)) {
+    when (hostInfo.hostName.lowercase(Locale.ROOT)) {
         "qq" -> {
             hostApp = HostApp.QQ
         }
@@ -49,20 +35,4 @@ fun init(applicationContext: Application) {
             hostApp = HostApp.WE_CHAT
         }
     }
-}
-
-private fun getHostInfo(context: Context): PackageInfo {
-    return try {
-        context.packageManager.getPackageInfo(context.packageName, PackageManager.GET_META_DATA)
-    } catch (thr: Throwable) {
-        Log.t(thr, "Utils--->Can not get PackageInfo!")
-        throw AssertionError("Can not get PackageInfo!")
-    }
-}
-
-@Suppress("DEPRECATION")
-private fun getLongVersionCode(info: PackageInfo): Long {
-    return if (Build.VERSION.SDK_INT >= 28) {
-        info.longVersionCode
-    } else info.versionCode.toLong()
 }
