f93a33d7d48b9b95089def505c4a5e89f9f9e2ec
Agoines <yb3412436097@gmail.com>
2022-03-07 02:24:36 +0800

    remove QQCleanerData.kt navigationBarHeight

--- app/src/main/java/me/kyuubiran/qqcleaner/QQCleanerData.kt
+++ app/src/main/java/me/kyuubiran/qqcleaner/QQCleanerData.kt
@@ -3,15 +3,12 @@ package me.kyuubiran.qqcleaner
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.unit.dp
 import me.kyuubiran.qqcleaner.ui.theme.QQCleanerColorTheme.getCurrentTheme
 import me.kyuubiran.qqcleaner.util.ConfigManager.sIsBlackTheme
 
 object QQCleanerData {
 
     var theme by mutableStateOf(getCurrentTheme())
-
-    var navigationBarHeight by mutableStateOf(0.dp)
 
     var isDark by mutableStateOf(false)
 
