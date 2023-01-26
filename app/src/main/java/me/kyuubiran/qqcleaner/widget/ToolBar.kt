760dcde1ccb0cd4b2368b41c98bd362911990784
agoines <yb3412436097@gmail.com>
2023-01-27 03:07:47 +0800

    add navigation anim

--- app/src/main/java/me/kyuubiran/qqcleaner/widget/ToolBar.kt
+++ app/src/main/java/me/kyuubiran/qqcleaner/widget/ToolBar.kt
@@ -8,6 +8,9 @@ import android.view.WindowInsets
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import androidx.core.view.updateLayoutParams
+import androidx.fragment.app.FragmentActivity
+import androidx.navigation.fragment.NavHostFragment
+import me.kyuubiran.qqcleaner.R
 import me.kyuubiran.qqcleaner.databinding.ToolbarBinding
 
 /**
@@ -16,13 +19,23 @@ import me.kyuubiran.qqcleaner.databinding.ToolbarBinding
  * @param attr 对应的 AttributeSet 参数
  */
 class ToolBar(context: Context, attr: AttributeSet) : LinearLayout(context, attr) {
+    private lateinit var text: String
+
     init {
+        initAttrs(attr)
         // 和对应的 xml 进行绑定
         val binding = ToolbarBinding.inflate(
             LayoutInflater.from(getContext()),
             this,
             true
         )
+        binding.titleText.text = text
+        binding.backIcon.setOnClickListener {
+            val fragment =
+                (this.context as FragmentActivity).supportFragmentManager.findFragmentById(R.id.nav_host_fragment)!!
+            val navController = NavHostFragment.findNavController(fragment)
+            navController.popBackStack()
+        }
 
         // 顶栏边距
         @Suppress("DEPRECATION")
@@ -35,5 +48,13 @@ class ToolBar(context: Context, attr: AttributeSet) : LinearLayout(context, attr
             }
             insets
         }
+    }
+
+    private fun initAttrs(attrs: AttributeSet) {
+        // 加载对应的 AttributeSet
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.ToolBar)
+        // 获取对应 text 属性的内容
+        text = typedArray.getString(R.styleable.ToolBar_title).toString()
+        typedArray.recycle()
     }
 }
